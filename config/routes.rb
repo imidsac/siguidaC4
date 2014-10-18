39827dcaf859a7bea575a9287abe158026a62298
imidsac <imidsac@hotmail.fr>
2014-10-18 12:28:10 +0000

    Conception des tables (centre, service, personnels, permission).

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :permissions
+
+  resources :personnels
+
+  resources :services
+
+  resources :centres
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
